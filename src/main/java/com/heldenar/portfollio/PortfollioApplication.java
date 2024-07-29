fa97a7d2decdee5e4aa928c89c00530efa41e0ef
Heldennar <krain7127@gmail.com>
2024-07-29 13:45:11 +0900

    init

---
+++ src/main/java/com/heldenar/portfollio/PortfollioApplication.java
@@ -0,0 +1,13 @@
+package com.heldenar.portfollio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfollioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfollioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/heldenar/portfollio/PortfollioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heldenar.portfollio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfollioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
